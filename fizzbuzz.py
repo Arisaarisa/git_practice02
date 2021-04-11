917cf690aac5902002f6d19f35e563509501cfa8
arisa <arisankochira10@gmail.com>
2021-04-11 22:58:36 +0900

    5の倍数挙動途中

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -2,8 +2,15 @@
 def fizzbuzz_convert(number):
     # もしnumberが3の倍数のとき
     if number % 3 == 0:
+        # Fizzと返す
         return 'Fizz'
+    # もしnumberが5の倍数のとき
+    if number % 5 == 0:
+        # Buzzと返す
+        return 'Buzz'
 
 
 # fizzbuzz_convertが3の時Fizz
 assert fizzbuzz_convert(3) == 'Fizz'
+# fizzbuzz_convertが5の時Buzz
+assert fizzbuzz_convert(5) == 'Buzz'
